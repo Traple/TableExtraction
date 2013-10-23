4687bdbb630539d36c620f7c63a91cd2a020333e
Sander <sander.van.boom@gmail.com>
2013-10-23 14:43:25 +0100

    The code can now separate the headers from the data per column

---
+++ src/main/java/Program3/Header.java
@@ -0,0 +1,22 @@
+package program3;
+
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+
+public class Header {
+    private ArrayList<ArrayList<Integer>> column;
+    private Elements spans;
+
+    public Header(Elements spans, ArrayList<ArrayList<Integer>> column){
+        this.spans = spans;
+        this.column = column;
+    }
+
+    /**
+     *
+     * @return
+     */
+
+
+}
---
+++ src/main/java/Program3/Page.java
@@ -0,0 +1,66 @@
+package program3;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created for project: TableExtraction
+ * In package: program2
+ * Created with IntelliJ IDEA.
+ * User: Sander van Boom
+ * Date: 15-10-13
+ * Time: 16:02
+ */
+/*
+ * The Page class is simply the page as it is being read by the main class. As soon as it detects a table it
+ * will create a Table class. It will parse the Table class it's own attributes to make sure the table can search in it's
+ * Location etc.
+ */
+public class Page {
+    private Elements spans;
+    //TODO: create a method for the detection of whitespace, so more information can be extracted succesfully.
+
+    public Page(String fileLocation) throws IOException {
+        //~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+        //Now we need to read the file:
+
+        File input = new File(fileLocation);
+        Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+
+        this.spans = doc.select("span.ocrx_word");
+    }
+
+    //This method will find the tables in the content. It creates a table object for every table it finds.
+    //The method returns null if it doesn't find a table.
+    //TODO: support multiple tables in the same page.
+    public Table createTables() throws IOException {
+        System.out.println("Going to create tables now!");
+        String word;
+        boolean foundATable = false;
+        Elements tableSpans = new Elements();
+        Table foundTable = null;
+        for (Element span : spans) {
+            word = span.text();
+            try {
+                if (word.substring(0, 5).equals("TABLE") || word.substring(0, 5).equals("table") || word.substring(0, 5).equals("Table")) {
+                    foundATable = true;
+                    tableSpans.add(span);
+                }
+            } catch (StringIndexOutOfBoundsException e) {
+                //do nothing
+            }
+            if (foundATable) {
+                tableSpans.add(span);
+            }
+        }
+        if (foundATable) {
+            foundTable = new Table(tableSpans);
+        }
+        return foundTable;
+    }
+}
